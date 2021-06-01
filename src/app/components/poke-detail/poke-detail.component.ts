b4c4c082dd04bdf0739b14ed7ca169a7e91d5505
Ross <rossfleming2711@gmail.com>
2021-06-01 14:55:06 +0100

    poke detail ts & html added

--- src/app/components/poke-detail/poke-detail.component.ts
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-poke-detail',
   templateUrl: './poke-detail.component.html',
-  styleUrls: ['./poke-detail.component.scss']
+  styleUrls: ['./poke-detail.component.scss'],
 })
 export class PokeDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private pokemonService: PokemonService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getPokemonDetail(params['id']);
+    });
   }
 
+  ngOnInit(): void {}
+
+  getPokemonDetail(id: number) {
+    this.pokemonService.getPokemons(id).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {}
+    );
+  }
 }
